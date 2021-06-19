745a35d897e9c61ae37464e2caee26ac99839cd2
BLasan <benuraab@gmail.com>
2021-06-19 07:47:48 +0530

    Fix: Checkstyle Issue in Loan Collateral Management Repository

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanCollateralManagementRepository.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanCollateralManagementRepository.java
@@ -18,12 +18,10 @@
  */
 package org.apache.fineract.portfolio.loanaccount.domain;
 
-import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface LoanCollateralManagementRepository
         extends JpaRepository<LoanCollateralManagement, Long>, JpaSpecificationExecutor<LoanCollateralManagement> {
